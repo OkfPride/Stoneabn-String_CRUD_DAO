fc7c1271ff72fec79e80450fff524c897e7142a0
JavaDev <my@email.com>
2021-03-08 16:40:40 +0300

    test2

--- src/main/java/com/konstantin/springmvc/springcrumdao/modelDAO/MyRowMapper.java
+++ src/main/java/com/konstantin/springmvc/springcrumdao/modelDAO/MyRowMapper.java
@@ -23,7 +23,7 @@ public class MyRowMapper implements RowMapper<Person> {
         person.setName(rs.getString("name"));
         person.setAge(rs.getInt("age"));
         person.setEmail(rs.getString("email"));
-        System.out.println("112");
+        System.out.println("112222222222");
         return person;
     }
 
